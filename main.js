022ac2ac89165a46585f693c7bc5bb20e54851b5
Alisson Corrales <alisson.corrales24@gmail.com>
2021-03-05 22:38:37 -0700

    finished ac 3/5/2021

--- main.js
+++ main.js
@@ -1,20 +1,7 @@
 `use strict`;
 
-//should make the percent go up
-//number is the percent in the string
-//final value is the number you get at the end
-function increase(number, finalValue){
-    //parseInt takes a string and grabs the integer
-    let currentVal = parseInt(document.getElementById(number).innerHTML, 10);
-    if(currentVal < finalValue){
-        currentVal++;
-        setTimeout(function(){increase(number,finalValue);}, 40);
-    }
-}
 
-//when you scroll down to the number, it will increase
-$("#1").(function(){
-    increase(1, 80)
+$('.navbar').css({
+    display: 'none'
 })
-
-//jquery here is heavily based off of Jacob Banner's work (https://codepen.io/jacobbanner/pen/dGZePo)
+$('.navbar').slideDown(1000);
