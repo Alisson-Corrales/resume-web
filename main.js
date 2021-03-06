d771b5d65f5c215a07966076e79fe097a7b8e6a9
Alisson Corrales <Acorra435@west-mec.local>
2021-03-05 18:25:15 -0700

    ac 3/3/2021

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 `use strict`;
 
-//$('.image').slideDown(1000)
+//should make the percent go up
+//number is the percent in the string
+//final value is the number you get at the end
+function increase(number, finalValue){
+    //parseInt takes a string and grabs the integer
+    let currentVal = parseInt(document.getElementById(number).innerHTML, 10);
+    if(currentVal < finalValue){
+        currentVal++;
+        setTimeout(function(){increase(number,finalValue);}, 40);
+    }
+}
+
+//when you scroll down to the number, it will increase
+$("#1").(function(){
+    increase(1, 80)
+})
+
+//jquery here is heavily based off of Jacob Banner's work (https://codepen.io/jacobbanner/pen/dGZePo)
